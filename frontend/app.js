967be56ad8298c3f9c3c7455b1e472f76f5091c4
Nick Kramer <krames12@gmail.com>
2016-09-05 13:15:45 -0400

    deleted folders

--- frontend/app.js
+++
@@ -1,6 +0,0 @@
-/*
-    ideas:
-        Warcraft Logs link / past three logs
-        possible login system to make notes on certain players
-        direct linking (.com/us/deathwing/saktap as an example)
-*/
