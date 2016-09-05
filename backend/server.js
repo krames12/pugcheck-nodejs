3f04d47c7ae644b396b094153540abb682e21d37
Nick Kramer <krames12@gmail.com>
2016-09-05 11:52:44 -0400

    filtered down progress data down to latest legion raids

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/thing', (req, res) => {
-   res.send('this is thing'); 
+   res.send('this is thing');
 });
 
 app.get('/us/:server/:characterName', (req, res) => {
@@ -22,22 +22,22 @@ app.use(function(req, res, next) {
 
 function getCharacterInfo(characterReq, characterRes, callback) {
     https.get('https://us.api.battle.net/wow/character/' + characterReq.params.server +  '/' + characterReq.params.characterName + '?fields=progression,items&locale=en_US&apikey=APIKEY', (res) => {
-        
+
         res.setEncoding('utf8');
-        
+
         console.log('statusCode:', res.statusCode);
         console.log('headers:', res.headers);
         console.log('Request made for ' + characterReq.params.characterName + ' on the server ' + characterReq.params.server);
-        
+
         // variable for incoming data
         var body = '';
-        
+
         // parses through data as it's recieved. buffer or not.
         res.on('data', (d) => {
             //process.stdout.write(d);
             body += d;
         });
-        
+
         // parses the recieved data and sends it to the callback function. also catches any errors.
         res.on('end', () => {
            try {
@@ -46,7 +46,7 @@ function getCharacterInfo(characterReq, characterRes, callback) {
                console.error('Unable to parse: ', err);
                return callback(err);
            }
-           
+
            callback(null, parsed, characterRes);
         });
 
@@ -57,14 +57,18 @@ function getCharacterInfo(characterReq, characterRes, callback) {
 
 function sortParsedData(err, data) {
     if (err) throw err;
-    
+
     var sortData = {
         "name": data.name,
         "realm": data.realm,
         "itemLevel": data.items.averageItemLevel,
-        "progress": data.progression.raids
+        "progress": data.progression.raids.filter((item, index) => {
+          if(item.name == "The Emerals Nightmare" || item.name == "The Nighthold") {
+            return item;
+          }
+        })
     }
-    
+
     return sortData;
 }
 
