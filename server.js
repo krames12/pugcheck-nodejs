4fda8f38022348901c3ae9aa39954785ac2d61a0
Nick Kramer <krames12@gmail.com>
2016-09-05 23:58:06 -0400

    404 handling works, added classIds objects for color info on render

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/thing', (req, res) => {
 });
 
 app.get('/us/:server/:characterName', (req, res) => {
-    getCharacterInfo(req, res, displayParsedData);
+      getCharacterInfo(req, res, displayParsedData);
 });
 
 app.use(function(req, res, next) {
@@ -46,19 +46,60 @@ function getCharacterInfo(characterReq, characterRes, callback) {
                console.error('Unable to parse: ', err);
                return callback(err);
            }
-
-           callback(null, parsed, characterRes);
+           console.log("statusCodev2: " + res.statusCode);
+           callback(null, parsed, characterReq, characterRes, res.statusCode);
         });
 
     }).on('error', (e) => {
-        console.error(e);
+        callback(e);
     });
 }
 
-function sortParsedData(err, data) {
-    if (err) throw err;
+var classIds = {
+  {
+    "id": 1,
+    "name": "warrior"
+  }, {
+    "id": 2,
+    "name": "paladin"
+  }, {
+    "id": 3,
+    "name": "hunter"
+  }, {
+    "id": 4,
+    "name": "rogue"
+  }, {
+    "id": 5,
+    "name": "priest"
+  }, {
+    "id": 6,
+    "name": "death-knight"
+  }, {
+    "id": 7,
+    "name": "shaman"
+  }, {
+    "id": 8,
+    "name": "mage"
+  }, {
+    "id": 9,
+    "name": "warlock"
+  }, {
+    "id": 10,
+    "name": "monk"
+  }, {
+    "id": 11,
+    "name": "druid"
+  }, {
+    "id": 12,
+    "name": "demon-hunter"
+  },
+}
 
-    var sortData = {
+function sortParsedData(err, data) {
+    if (err) {
+      return err;
+    } else {
+      var sortData = {
         "name": data.name,
         "realm": data.realm,
         "itemLevel": data.items.averageItemLevel,
@@ -67,16 +108,20 @@ function sortParsedData(err, data) {
             return item;
           }
         })
-    }
+      }
 
-    return sortData;
+      return sortData;
+    }
 }
 
-function displayParsedData(err, data, originRes) {
+function displayParsedData(err, data, originReq, originRes, statusCode) {
     if (err) throw err;
-    var sortedData = sortParsedData(err, data);
-    console.log('sortedData: ' + sortedData);
-    originRes.send(sortedData);
+    if (statusCode !== 404){
+      var sortedData = sortParsedData(err, data);
+      originRes.send(sortedData);
+    } else {
+      originRes.send("The character " + originReq.params.characterName + " does not exist on " + originReq.params.server);
+    }
 }
 
 app.listen(8080, () => {
