baa07c65c0426c840024f560e117e3b23027f37c
Nick Kramer <krames12@gmail.com>
2017-01-16 23:12:54 +0000

    added NH boss id's to lookup

--- utils/lookups.js
+++ utils/lookups.js
@@ -38,7 +38,19 @@ const bossMap = {
   // Trial of Valor
   "Odyn": 1958,
   "Guarm": 1962,
-  "Helya": 2008
+  "Helya": 2008,
+  
+  // The Nighthold
+  "Skorpyron": 1849,
+  "Chronomatic Anomaly": 1865,
+  "Trilliax": 1867,
+  "Spellblade Aluriel": 1871,
+  "Tichondrius": 1862,
+  "Star Augur Etraeus": 1863,
+  "Krosus": 1842,
+  "High Botanist Tel'arn": 1886,
+  "Grand Magistrix Elisande": 1872,
+  "Gul'dan": 1866
 };
 
 const lookupBossId = bossName => {
